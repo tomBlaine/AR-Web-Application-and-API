21be4076f9583aaf0a84e9fb9ed3854742d7c099
tomBlaine <tom.blaine2002@gmail.com>
2023-02-16 19:41:28 +0000

    test

--- resources/views/presentations/create.blade.php
+++ resources/views/presentations/create.blade.php
@@ -1,21 +1,11 @@
 
 @extends('layouts.basic')
 
-@section('title', 'Timeline')
+@section('title', 'Create')
 
 @section('content')
 
 <p>Create Presentation:</p>
-<form method="POST" action="{{route('presentation.store')}}">
-    @csrf
-    <p>Title: </p>
-    <textarea type="text" name="title" rows=2 style="width: 80%"></textarea>
-    <p>Body: </p>
-    <textarea type="text" name="body" rows=15 style="width: 80%"></textarea>
 
-    <br>
-    <br>
-    <input type="submit" value="Post">
-</form>
 
 @endsection
