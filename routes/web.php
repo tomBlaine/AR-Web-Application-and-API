0bda67f6d6fb68f2828eff5721dc91a7a28e3984
tomBlaine <tom.blaine2002@gmail.com>
2023-02-18 11:50:35 +0000

    added controller ref

--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,7 @@
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PresentationController;
-
+use App\Http\Controllers\SlideController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
