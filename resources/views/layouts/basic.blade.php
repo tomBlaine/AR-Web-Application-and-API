bc9347009cdea2a8f22b486d8985988dd048de6b
tomBlaine <tom.blaine2002@gmail.com>
2023-02-18 12:02:03 +0000

    added error messages and type=text

--- resources/views/layouts/basic.blade.php
+++ resources/views/layouts/basic.blade.php
@@ -1,6 +1,16 @@
 <!DOCTYPE html>
 <body>
     <div>
+        @if($errors->any())
+        <div>
+            Errors:
+            <ul>
+                @foreach($errors->all() as $error)
+                    <li>{{$error}}</li>
+                @endforeach
+            </ul>
+        </div>
+        @endif
         @yield('content')
     </div>
 </body>
--- resources/views/slides/create.blade.php
+++ resources/views/slides/create.blade.php
@@ -5,13 +5,13 @@
 <form method="POST" action="{{route('slides.store', ['id'=>$presentation])}}">
     @csrf
     <p>Text1: </p>
-    <textarea name="text1" rows=3 style="width: 30%"></textarea>
+    <textarea type="text" name="text1" rows=3 style="width: 30%"></textarea>
     <p>Text2: </p>
-    <textarea name="text2" rows=3 style="width: 30%"></textarea>
+    <textarea type="text" name="text2" rows=3 style="width: 30%"></textarea>
     <p>Text3: </p>
-    <textarea name="text3" rows=3 style="width: 30%"></textarea>
+    <textarea type="text" name="text3" rows=3 style="width: 30%"></textarea>
     <p>Obj Ref: </p>
-    <textarea name="obj" rows=3 style="width: 30%"></textarea>
+    <textarea  type="text" name="obj" rows=3 style="width: 30%"></textarea>
     <input type="submit" value="Save">
     <a href="{{route('presentations.index')}}">Cancel</a>
 </form>
