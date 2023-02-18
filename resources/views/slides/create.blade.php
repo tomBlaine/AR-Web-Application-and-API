65f17bf0a17e64a3844b4ae4d44b94046baa6944
tomBlaine <tom.blaine2002@gmail.com>
2023-02-18 11:48:54 +0000

    removed type

--- resources/views/slides/create.blade.php
+++ resources/views/slides/create.blade.php
@@ -5,13 +5,13 @@
 <form method="POST" action="{{route('slides.store', ['id'=>$presentation])}}">
     @csrf
     <p>Text1: </p>
-    <textarea type="text" name="text1" rows=3 style="width: 30%"></textarea>
+    <textarea name="text1" rows=3 style="width: 30%"></textarea>
     <p>Text2: </p>
-    <textarea type="text" name="text2" rows=3 style="width: 30%"></textarea>
+    <textarea name="text2" rows=3 style="width: 30%"></textarea>
     <p>Text3: </p>
-    <textarea type="text" name="text3" rows=3 style="width: 30%"></textarea>
+    <textarea name="text3" rows=3 style="width: 30%"></textarea>
     <p>Obj Ref: </p>
-    <textarea type="text" name="obj" rows=3 style="width: 30%"></textarea>
+    <textarea name="obj" rows=3 style="width: 30%"></textarea>
     <input type="submit" value="Save">
     <a href="{{route('presentations.index')}}">Cancel</a>
 </form>
