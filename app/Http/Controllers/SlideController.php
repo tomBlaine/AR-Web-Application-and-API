4dee999cf925a572aa5efe48c856d8198225a619
tomBlaine <tom.blaine2002@gmail.com>
2023-02-18 11:44:33 +0000

    auth ref

--- app/Http/Controllers/SlideController.php
+++ app/Http/Controllers/SlideController.php
@@ -6,6 +6,7 @@ use Illuminate\Http\Request;
 use App\Models\User;
 use App\Models\Presentation;
 use App\Models\Slide;
+use Illuminate\Support\Facades\Auth;
 
 class SlideController extends Controller
 {
