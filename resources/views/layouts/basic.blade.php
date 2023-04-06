700f5f17fffc3cafe563ed30a6cf4d8f1f2ab4ae
tomBlaine <tom.blaine2002@gmail.com>
2023-04-06 22:58:46 +0100

    added webpack mix

--- resources/views/layouts/basic.blade.php
+++ resources/views/layouts/basic.blade.php
@@ -2,8 +2,10 @@
 @vite('resources/css/app.css')
 @vite('resources/js/app.js')
 <head>
-    <meta charset="utf-8">
-    <link href="{{asset('css/app.css')}}" rel="stylesheet">
+
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link href="/css/app.css" rel="stylesheet">
 </head>
 <body>
     <div>
