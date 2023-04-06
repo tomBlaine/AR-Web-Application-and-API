9ddef747b652a3b096d1a4b9a127845236d92eac
tomBlaine <tom.blaine2002@gmail.com>
2023-04-06 23:16:21 +0100

    added referetr policy

--- resources/views/presentations/show.blade.php
+++ resources/views/presentations/show.blade.php
@@ -168,7 +168,7 @@
 
 
 <textarea class="description" name="description"></textarea>
-<script src="https://cdn.tiny.cloud/1/no-api-key/tinymce/6/tinymce.min.js"></script>
+<script src="https://cdn.tiny.cloud/1/no-api-key/tinymce/6/tinymce.min.js" referrerpolicy="origin"></script>
 <script>
 tinymce.init({
     selector:'textarea.description',
