1d11cf1a12b96543a7cb4ea533b0816a9ebec622
tomBlaine <tom.blaine2002@gmail.com>
2023-04-09 14:04:10 +0100

    removed media embedd

--- resources/views/presentations/show.blade.php
+++ resources/views/presentations/show.blade.php
@@ -208,7 +208,30 @@
             grid9: document.getElementById( 'grid9' ),
 
 
-        },)
+        },
+        
+        {
+            cloudServices: {
+                // All predefined builds include the Easy Image feature.
+                // Provide correct configuration values to use it.
+                //tokenUrl: 'https://example.com/cs-token-endpoint',
+                //uploadUrl: 'https://your-organization-id.cke-cs.com/easyimage/upload/'
+                // Read more about Easy Image - https://ckeditor.com/docs/ckeditor5/latest/features/images/image-upload/easy-image.html.
+                // For other image upload methods see the guide - https://ckeditor.com/docs/ckeditor5/latest/features/images/image-upload/image-upload.html.
+            },
+            toolbar: {
+                items: [
+                    'undo', 'redo',
+                    '|', 'heading',
+                    '|', 'bold', 'italic',
+                    '|', 'link', 'uploadImage', 'insertTable',
+                    '|', 'bulletedList', 'numberedList', 'outdent', 'indent'
+                ]
+            }
+        }
+        
+        
+        )
         .then( editor => {
             window.editor = editor;
 
