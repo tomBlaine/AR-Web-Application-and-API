e57f112e4aacfd2b42c8d9ef0fe1dc455489ac32
tomBlaine <tom.blaine2002@gmail.com>
2023-04-11 15:18:17 +0100

    obj box is now tailwind

--- resources/views/presentations/show.blade.php
+++ resources/views/presentations/show.blade.php
@@ -199,9 +199,9 @@
                         
                           <div class="editor1" id="grid1" name="grid1" style="width=100%; height=200px"></div>
                         </div>
-                        <!-- Right side with large image -->
+
                         <div style="flex: 1;">
-                          <img src="image.jpg" alt="Large Image" style="width: 100%; height: auto;">
+                          <img src="image.jpg" alt="Large Image" style="width: 100%; height: 300px;">
                         </div>
                       </div>
                     </div>
@@ -228,8 +228,24 @@
                     <br>
 
                     <br>
-                    <p>Obj Ref: </p>
-                    <textarea  type="text" name="obj" rows=3 style="width: 30%"></textarea>
+                    <p>3D Model Link (from sketchfab): </p>
+                    
+                    <div class="relative mb-3 xl:w-96" data-te-input-wrapper-init>
+                      <textarea
+                        class="peer block min-h-[auto] w-full rounded border-0 bg-transparent px-3 py-[0.32rem] leading-[1.6] outline-none transition-all duration-200 ease-linear focus:placeholder:opacity-100 data-[te-input-state-active]:placeholder:opacity-100 motion-reduce:transition-none dark:text-neutral-200 dark:placeholder:text-neutral-200 [&:not([data-te-input-placeholder-active])]:placeholder:opacity-0"
+                        id="objID"
+                        rows="1"
+                        name="obj"
+                        type="text"
+                        placeholder="Your sketchfab link"></textarea>
+                      <label
+                        for="exampleFormControlTextarea1"
+                        class="pointer-events-none absolute left-3 top-0 mb-0 max-w-[90%] origin-[0_0] truncate pt-[0.37rem] leading-[1.6] text-neutral-500 transition-all duration-200 ease-out peer-focus:-translate-y-[0.9rem] peer-focus:scale-[0.8] peer-focus:text-primary peer-data-[te-input-state-active]:-translate-y-[0.9rem] peer-data-[te-input-state-active]:scale-[0.8] motion-reduce:transition-none dark:text-neutral-200 dark:peer-focus:text-primary"
+                        >Sketchfab Link</label
+                      >
+                    </div>
+
+
                     <input type="submit" value="Save">
                     <a href="{{route('presentations.index')}}">Cancel</a>
                 </form>
