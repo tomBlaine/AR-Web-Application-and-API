6360a37b2c78e601df9a295200cce8c24a5b596b
tomBlaine <tom.blaine2002@gmail.com>
2023-04-11 21:49:43 +0100

    added null value for colour choice

--- resources/views/presentations/show.blade.php
+++ resources/views/presentations/show.blade.php
@@ -304,7 +304,7 @@
                           <br>
                           <label for="boxColour3" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Select a box colour:</label>
                           <select id="boxColour3" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
-                            <option selected>Choose a colour</option>
+                            <option selected value="null">Choose a colour</option>
                             <option value="green">Light Green</option>
                             <option value="purple">Light Purple</option>
                             <option value="white">White</option>
@@ -337,9 +337,9 @@
                     <input type="hidden" name="grid2_data" id="grid2_data" value="null">
                     <input type="hidden" name="grid3_data" id="grid3_data" value="null">
                     
-                    <input type="hidden" name="colorSelection1" id="colorSelection1" value="">
-                    <input type="hidden" name="colorSelection2" id="colorSelection2" value="">
-                    <input type="hidden" name="colorSelection3" id="colorSelection3" value="">
+                    <input type="hidden" name="colorSelection1" id="colorSelection1" value="null">
+                    <input type="hidden" name="colorSelection2" id="colorSelection2" value="null">
+                    <input type="hidden" name="colorSelection3" id="colorSelection3" value="null">
                     
                     <br>
 
