fc6285e8cf838add1b112524b0e24c1e81db49fc
tomBlaine <tom.blaine2002@gmail.com>
2023-04-12 23:18:35 +0100

    fixed

--- app/Http/Controllers/SlideController.php
+++ app/Http/Controllers/SlideController.php
@@ -86,10 +86,11 @@ class SlideController extends Controller
 
     public function destroy($id)
     {
-        $presentationID = $slide->pres_ID;
+        
         $slide = Slide::findOrFail($id);
+        $presentationID = $slide->pres_ID;
         $slide->delete();
-        return redirect()->route('presentations.show', ['$id'=>$presentationID])->with('message', 'slide was deleted.');
+        return redirect()->route('presentations.show', ['id'=>$presentationID])->with('message', 'slide was deleted.');
     }
 
     
