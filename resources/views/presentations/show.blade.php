69babf397e5e89cf9d868e8057c799338c7f0fbe
tomBlaine <tom.blaine2002@gmail.com>
2023-04-14 17:54:25 +0100

    added jumbrotron

--- resources/views/presentations/show.blade.php
+++ resources/views/presentations/show.blade.php
@@ -45,14 +45,24 @@
 @endphp
 <body>
 
-<div>
-<p>Title: {{$presentation->title}}</p>
-<p>Description: {{$presentation->text}}</p>
-<p>User: {{$presentation->User->name}}</p>
-<br>
-
-
-
+<div
+  class="rounded-lg bg-neutral-100 p-6 text-neutral-700 shadow-lg dark:bg-neutral-600 dark:text-neutral-200 dark:shadow-black/30">
+  <h2 class="mb-5 text-3xl font-semibold">{{$presentation->title}}</h2>
+  <p>
+    @ {{$presentation->User->name}}
+  </p>
+  <hr
+    class="my-6 h-0.5 border-t-0 bg-neutral-200 opacity-100 dark:opacity-30" />
+  <p class="mb-4">
+    {{$presentation->text}}
+  </p>
+  <button
+    type="button"
+    data-te-ripple-init
+    data-te-ripple-color="light"
+    class="rounded bg-primary px-6 pb-2 pt-2.5 text-xs font-medium uppercase leading-normal text-white shadow-[0_4px_9px_-4px_#3b71ca] transition duration-150 ease-in-out hover:bg-primary-600 hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:bg-primary-600 focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:outline-none focus:ring-0 active:bg-primary-700 active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] dark:shadow-[0_4px_9px_-4px_rgba(59,113,202,0.5)] dark:hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)] dark:focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)] dark:active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)]">
+    Click to Deploy Now!
+  </button>
 </div>
 
 
