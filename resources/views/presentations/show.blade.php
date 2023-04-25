3ecaff637b476e9b689a917eb058b10de66c7805
tomBlaine <tom.blaine2002@gmail.com>
2023-04-25 16:05:11 +0100

    button

--- resources/views/presentations/show.blade.php
+++ resources/views/presentations/show.blade.php
@@ -46,7 +46,7 @@
 <body>
 
 <div
-  style="border-radius: 5px; background-color: #f1f1f19c;"
+  style="border-radius: 5px; background-color: #ecf1ff;"
   class="rounded-lg bg-neutral-100 p-6 text-neutral-700 shadow-lg dark:bg-neutral-600 dark:text-neutral-200 dark:shadow-black/30">
   <h2 class="mb-5 text-3xl font-semibold">{{$presentation->title}}</h2>
   <p>
@@ -61,6 +61,7 @@
     type="button"
     data-te-ripple-init
     data-te-ripple-color="light"
+    style="background-color: #386dfd; color: white;"
     class="rounded bg-primary px-6 pb-2 pt-2.5 text-xs font-medium uppercase leading-normal shadow-[0_4px_9px_-4px_#3b71ca] transition duration-150 ease-in-out hover:bg-primary-600 hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:bg-primary-600 focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:outline-none focus:ring-0 active:bg-primary-700 active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] dark:shadow-[0_4px_9px_-4px_rgba(59,113,202,0.5)] dark:hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)] dark:focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)] dark:active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)]">
     Click to Deploy Now!
   </button>
@@ -79,7 +80,7 @@
           type="button"
           data-te-collapse-init
           data-te-target="#collapse{{$count}}5"
-          style="background-color: #efefef"
+          style="background-color: #ffffff"
           aria-expanded="false"
 
           aria-controls="collapse{{$count}}5">
