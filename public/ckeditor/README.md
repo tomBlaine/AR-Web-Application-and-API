55872044f0cbfd3267bba60a9fce66de4c94b48e
tomBlaine <tom.blaine2002@gmail.com>
2023-04-25 17:35:27 +0100

    new ckeditor

--- public/ckeditor/README.md
+++ public/ckeditor/README.md
@@ -57,7 +57,7 @@ Follow the guides available on https://ckeditor.com/docs/ckeditor5/latest/framew
 ## FAQ
 | Where is the place to report bugs and feature requests?
 
-You can create an issue on https://github.com/ckeditor/ckeditor5/issues including the build id - `qr0wowhu05ri-ycwdx5r1c3oi`. Make sure that the question / problem is unique, please look for a possibly asked questions in the search box. Duplicates will be closed.
+You can create an issue on https://github.com/ckeditor/ckeditor5/issues including the build id - `nonbko8ekwu8-khp31ilwu5ga`. Make sure that the question / problem is unique, please look for a possibly asked questions in the search box. Duplicates will be closed.
 
 | Where can I learn more about the CKEditor 5 framework?
 
--- public/ckeditor/src/ckeditor.js
+++ public/ckeditor/src/ckeditor.js
@@ -3,45 +3,23 @@
  * For licensing, see LICENSE.md or https://ckeditor.com/legal/ckeditor-oss-license
  */
 import BalloonEditor from '@ckeditor/ckeditor5-editor-balloon/src/ballooneditor.js';
-import Autoformat from '@ckeditor/ckeditor5-autoformat/src/autoformat.js';
-import BlockQuote from '@ckeditor/ckeditor5-block-quote/src/blockquote.js';
 import Bold from '@ckeditor/ckeditor5-basic-styles/src/bold.js';
-import CloudServices from '@ckeditor/ckeditor5-cloud-services/src/cloudservices.js';
 import Essentials from '@ckeditor/ckeditor5-essentials/src/essentials.js';
-import FontColor from '@ckeditor/ckeditor5-font/src/fontcolor.js';
-import FontFamily from '@ckeditor/ckeditor5-font/src/fontfamily.js';
 import FontSize from '@ckeditor/ckeditor5-font/src/fontsize.js';
-import Heading from '@ckeditor/ckeditor5-heading/src/heading.js';
 import Highlight from '@ckeditor/ckeditor5-highlight/src/highlight.js';
-import Image from '@ckeditor/ckeditor5-image/src/image.js';
-import ImageUpload from '@ckeditor/ckeditor5-image/src/imageupload.js';
-import Indent from '@ckeditor/ckeditor5-indent/src/indent.js';
 import Italic from '@ckeditor/ckeditor5-basic-styles/src/italic.js';
-import List from '@ckeditor/ckeditor5-list/src/list.js';
 import Paragraph from '@ckeditor/ckeditor5-paragraph/src/paragraph.js';
-import Table from '@ckeditor/ckeditor5-table/src/table.js';
 
 class Editor extends BalloonEditor {}
 
 // Plugins to include in the build.
 Editor.builtinPlugins = [
-	Autoformat,
-	BlockQuote,
 	Bold,
-	CloudServices,
 	Essentials,
-	FontColor,
-	FontFamily,
 	FontSize,
-	Heading,
 	Highlight,
-	Image,
-	ImageUpload,
-	Indent,
 	Italic,
-	List,
-	Paragraph,
-	Table
+	Paragraph
 ];
 
 // Editor configuration.
@@ -50,20 +28,9 @@ Editor.defaultConfig = {
 		items: [
 			'bold',
 			'italic',
+			'|',
 			'fontSize',
-			'fontColor',
-			'fontFamily',
 			'highlight',
-			'|',
-			'bulletedList',
-			'numberedList',
-			'|',
-			'outdent',
-			'indent',
-			'|',
-			'imageUpload',
-			'blockQuote',
-			'insertTable',
 			'|',
 			'undo',
 			'redo'
